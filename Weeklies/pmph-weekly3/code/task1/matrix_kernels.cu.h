0a2e5e8e386b2343f55ea9a7170eab151d0eda35
jqp221 <jqp221@a00330.science.domain>
2015-10-04 23:43:39 +0200

    Added naive kernel.

--- Weeklies/pmph-weekly3/code/task1/matrix_kernels.cu.h
+++ Weeklies/pmph-weekly3/code/task1/matrix_kernels.cu.h
@@ -4,8 +4,26 @@
 #include "matrix.cu.h"
 
 template <class T>
+__device__ float getElement(matrix_t<T> mat, int i, int j) {
+  return mat.elements[mat.width * i + j];
+}
+
+template <class T>
+__device__ void setElement(matrix_t<T> mat, int i, int j, T val) {
+  mat.elements[mat.width * i + j] = val;
+}
+
+template <class T>
 __global__ void
 matrix_transpose_naive_kernel(matrix_t<T> d_out, matrix_t<T> d_in) {
+  const unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
+  const unsigned int y = blockIdx.y*blockDim.y + threadIdx.y;
+
+  if (x >= d_in.width || y >= d_in.height)
+    return;
+
+  T e = getElement(d_in, y, x);
+  setElement(d_out, x, y);
 }
 
 #endif
